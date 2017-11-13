644178d22a78b136016dafd4bd599cc2dcf4260a
Trairat Punyawatthananukool <samoooop@gmail.com>
2017-11-13 21:45:51 +0700

    Fix to use expected reward of current policy to train model

--- CartPole.py
+++ CartPole.py
@@ -47,15 +47,24 @@ def getModel(action_count):
     model.add(Dense(action_count))
     return model
 
-def getTrainData(histories, expected_rewards, reward):
-    x = np.array(histories)
-    # replace maximum reward with actual reward(maximum = action selected)
-    expected_rewards = np.array(expected_rewards)
+def getTrainData(histories, rewards, actions, dones, new_states):
+    expected_rewards = model.predict(histories)
+    expected_rewards_from_next_step = model.predict(new_states)
+    expected_rewards_from_next_step = np.max(expected_rewards_from_next_step, axis = 1)
+    # print('z')
+    # actions_index = np.array(actions)
+    not_dones = np.logical_not(dones)
+    # print(dones,type(not_dones),type(done))
+    # print(not_dones,expected_rewards_from_next_step.shape,rewards.shape)
+    # print(expected_rewards_from_next_step.shape,rewards.shape)
+    expected_rewards[dones, actions[dones]] = rewards[dones]
+    expected_rewards[not_dones, actions[not_dones]] = (rewards[not_dones] 
+                                                +  l * expected_rewards_from_next_step[not_dones])
     # print('xxx',expected_rewards.shape)
-    maxi = np.argmax(expected_rewards, axis = 1)
-    expected_rewards[:,maxi] = reward
+    # maxi = np.argmax(expected_rewards, axis = 1)
+    # expected_rewards[:,maxi] = reward
     # print('xxx',expected_rewards.shape)
-    return x, expected_rewards
+    return expected_rewards
     
 load = True
 env_name = 'CartPole-v0'
@@ -83,13 +92,15 @@ memory_size = 100
 histories = []
 actions = []
 rewards = []
+dones = []
+new_states = []
 action_picked = np.zeros(6)
 last_reward = np.zeros(10,dtype = np.float)
 i = 0
 j = 0
-e = 0.05
+e = 0.0
 decay_rate = 1.0
-l = 1.0
+l = 0.9
 learning_rate = 0.2
 x = np.stack((obs, obs), axis = 0)
 print(np.array([x]))
@@ -110,20 +121,22 @@ while True:
     new_state = np.concatenate((state[0][1:], np.array([obs])), axis = 0)
     new_state = np.array([new_state])
 
-    if done:
-        expected_reward[action] = reward
-    else:
-        # xp = np.array([prep(obs)])
-        # xp = np.array([obs.flatten()])
-        expected_reward_from_next_step = np.max(model.predict(new_state)[0])
-        expected_reward[action] = reward + l*expected_reward_from_next_step
+    # if done:
+    #     expected_reward[action] = reward
+    # else:
+    #     # xp = np.array([prep(obs)])
+    #     # xp = np.array([obs.flatten()])
+    #     expected_reward_from_next_step = np.max(model.predict(new_state)[0])
+    #     expected_reward[action] = reward + l*expected_reward_from_next_step
 
-    if i%20 == 0:
-        env.render()
+    # if i%20 == 0:
+    # env.render()
 
     actions.append(action)
-    rewards.append(expected_reward)
+    rewards.append(reward)
     histories.append(state[0])
+    dones.append(done)
+    new_states.append(new_state[0])
     # actions.append(action)
 
     state = new_state
@@ -142,13 +155,23 @@ while True:
         # fit NN
         x = np.array(histories)
         y = np.array(rewards)
+        a = np.array(actions)
+        d = np.array(dones)
+        ns = np.array(new_states)
+        # print('x')
         if x.shape[0] >= 1000:
             choices = np.random.choice(x.shape[0],1000)
             x = x[choices]
             y = y[choices]
+            a = a[choices]
+            d = d[choices]
+            ns = ns[choices]
+        # print('y')
+        y = getTrainData(x, y, a, d, ns)
+
         print('training with x:',x.shape,' y:',y.shape)
             # x,y = getTrainData(histories, rewards, total_reward)
-        model.fit(x,y,batch_size = 64, epochs = 1, verbose = 0)
+        model.fit(x,y,batch_size = 64, epochs = 10, verbose = 0)
         score = model.evaluate(x, y, verbose = 0)
         print('show some prediction')
         choices = np.random.choice(x.shape[0], 5)
